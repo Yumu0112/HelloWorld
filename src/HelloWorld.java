1ee955d05a06f2bae3f4cc55b785232dedd8f695
Yumu0112 <yumu0112@icloud.com>
2023-06-13 16:17:29 +0900

    第一回課題

---
+++ src/HelloWorld.java
@@ -0,0 +1,41 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+        System.out.println("Hello,World");
+
+        //while practice
+        //		int i = 0;
+        //		while (i <= 5) {
+        //			System.out.println("Hello, World" + i);
+        //			i = i + 1;
+        //	}
+        //		System.out.println("last" + i);
+        //	}
+
+        // for practice
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello, World");
+        }
+
+        //Array + for
+        //		String[] color = {"red", "green", "blue"};
+        //		System.out.println(color.length);
+        //
+        //		for (int i=0; i<color.length; i++) {
+        //			System.out.println(color[i]);
+        //		}
+        //	}
+
+        //if + for
+
+        for (int i = 0; i <= 15; i++) {
+            if (i <= 10) {
+                System.out.println("Hello, World");
+            } else {
+                System.out.println("Hello");
+            }
+        }
+    }
+}
